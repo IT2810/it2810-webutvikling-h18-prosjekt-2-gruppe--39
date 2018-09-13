df0fef0082bf823d714252bfb3e0195907071095
Mikkel Sandsbraten <mikkel.sandsbraten@gmail.com>
2018-09-13 23:48:37 +0200

    completed poems fetch with ajax, design of sidebar in progress, design of cards with poems in progress

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Card from "./components/Card/Card";
+
+const openStyle = {
+  gridTemplateColumns: "250px auto 25px"
+};
+const closedStyle = {
+  gridTemplateColumns: "25px auto 25px"
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleMenuToggle = this.handleMenuToggle.bind(this);
+    this.state = {
+      isMenuOpen: false
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        {this.state.isMenuOpen ? (
+          <div className="sidebar-wrapper" style={openStyle}>
+            <Sidebar callback={this.handleMenuToggle} />
+          </div>
+        ) : (
+          <span
+            className="hamburger"
+            onClick={() => this.handleMenuToggle()}
+            style={closedStyle}
+          >
+            &#9776;
+          </span>
+        )}
+        <div className="card-wrapper">
+          <Card className="card" />
+          <Card className="card" />
+          <Card className="card" />
+          <Card className="card" />
+          <Card className="card" />
+        </div>
       </div>
     );
+  }
+
+  handleMenuToggle() {
+    this.setState({ isMenuOpen: !this.state.isMenuOpen });
   }
 }
 
